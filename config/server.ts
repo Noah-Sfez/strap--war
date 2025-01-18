5311bb162a7d1c7fc5a373d1c729860694860c84
Noah Sfez <sfz.noah@gmail.com>
2025-01-18 16:10:59 +0100

    ajout variable environnement

--- config/server.ts
+++ config/server.ts
@@ -1,7 +1,9 @@
 export default ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  app: {
-    keys: env.array('APP_KEYS'),
-  },
+    host: env("HOST", "0.0.0.0"),
+    port: env.int("PORT", 1337),
+    app: {
+        keys: env.array(
+            "JU1E7/8ZLQnfXZCz/1oHqg==,2XuV6zvaq/lQ1ptRX2F6YQ==,fE/+iQwJkZswpoMQNPijDg==,tP6h0CEKZLGN6ID/RLfeGg=="
+        ),
+    },
 });
