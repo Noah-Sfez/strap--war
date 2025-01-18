eaeff61fe2af6c83f55893b3f073d3f8470784cf
Noah Sfez <sfz.noah@gmail.com>
2025-01-18 17:49:28 +0100

    modification server

--- config/server.ts
+++ config/server.ts
@@ -1,7 +1,14 @@
-module.exports = ({ env }) => ({
-    host: env("HOST", "0.0.0.0"),
-    port: env.int("PORT", process.env.PORT),
-    app: {
-        keys: env.array("APP_KEYS"),
-    },
-});
+module.exports = ({ env }) => {
+    const host = env("HOST", "0.0.0.0");
+    const port = process.env.PORT || env.int("PORT", 1337);
+
+    console.log(`Strapi is running on host: ${host} and port: ${port}`);
+
+    return {
+        host,
+        port,
+        app: {
+            keys: env.array("APP_KEYS"),
+        },
+    };
+};
