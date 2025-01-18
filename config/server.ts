4a131204447a3b06facaaac59dbc896baeceb562
Noah Sfez <sfz.noah@gmail.com>
2025-01-18 17:35:04 +0100

    ajout du port

--- config/server.ts
+++ config/server.ts
@@ -1,12 +1,7 @@
-export default ({ env }) => ({
+module.exports = ({ env }) => ({
     host: env("HOST", "0.0.0.0"),
     port: env.int("PORT", 1337),
     app: {
-        keys: [
-            "JU1E7/8ZLQnfXZCz/1oHqg==",
-            "2XuV6zvaq/lQ1ptRX2F6YQ==",
-            "fE/+iQwJkZswpoMQNPijDg==",
-            "tP6h0CEKZLGN6ID/RLfeGg==",
-        ],
+        keys: env.array("APP_KEYS"),
     },
 });
